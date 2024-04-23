760f16574a0ef457057a5d1a144ef196e52b3b7b
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-04-24 00:56:38 +0530

    first commit

---
+++ client/src/components/Input.jsx
@@ -0,0 +1,12 @@
+export default function Input({ placeholder, handleInput, name }) {
+  return (
+    <div>
+      <input
+        name={name}
+        onChange={handleInput}
+        className="input-field"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
